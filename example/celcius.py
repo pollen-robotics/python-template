42505fcf82f3c64075a55f8d7377758bb6469dd4
Fabien Danieau <fabien.danieau@pollen-robotics.com>
2023-02-23 17:58:57 +0100

    fix #9: fix coding convention. private attribute needs a _

--- example/celcius.py
+++ example/celcius.py
@@ -5,20 +5,20 @@ class Celsius:
     """Manage celcius temperature and other format."""
 
     def __init__(self, temperature: float = 0):
-        self.logger = logging.getLogger(__name__)
-        self.temperature = temperature
+        self._logger = logging.getLogger(__name__)
+        self._temperature = temperature
 
     def to_fahrenheit(self):
-        return (self.temperature * 1.8) + 32
+        return (self._temperature * 1.8) + 32
 
     @property
     def temperature(self):
-        self.logger.info("Getting value...")
+        self._logger.info("Getting value...")
         return self._temperature
 
     @temperature.setter
     def temperature(self, value):
-        self.logger.info("Setting value...")
+        self._logger.info("Setting value...")
         if value < -273.15:
             raise ValueError("Temperature below -273 is not possible")
         self._temperature = value
