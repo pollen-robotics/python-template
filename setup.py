55b2a288eac6d15c6b9db5300414cc7366e4c561
RemiFabre <remifabre1800@gmail.com>
2023-04-26 17:15:36 +0200

    fixes #14 fixes #3 Black format

--- setup.py
+++ setup.py
@@ -8,31 +8,27 @@ from setuptools import find_packages, setup
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 
 setup(
-    name='python-template',
-    version='0.1.0',
-    packages=find_packages(exclude=['tests']),
-
+    name="python-template",
+    version="0.1.0",
+    packages=find_packages(exclude=["tests"]),
     install_requires=[
-        'black',
-        'flake8',
-        'pytest',
-        'numpy',
+        "black",
+        "flake8",
+        "pytest",
+        "numpy",
     ],
-
     extras_require={
-        'doc': ['sphinx'],
+        "doc": ["sphinx"],
     },
-
-    author='Pollen Robotics',
-    author_email='contact@pollen-robotics.com',
-    url='https://github.com/pollen-robotics/reachy-sdk',
-
-    description='Python template project.',
+    author="Pollen Robotics",
+    author_email="contact@pollen-robotics.com",
+    url="https://github.com/pollen-robotics/reachy-sdk",
+    description="Python template project.",
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
 )
